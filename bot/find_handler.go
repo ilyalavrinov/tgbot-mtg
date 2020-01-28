6163d9bf12b2b054233784b46289b85fc8d6b5f3
Ilya Lavrinov <ilya.v.lavrinov@gmail.com>
2020-01-28 23:58:37 +0300

    Initial dump load

--- bot/find_handler.go
+++ bot/find_handler.go
@@ -1,26 +1,101 @@
 package bot
 
 import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path"
+
 	"github.com/admirallarimda/tgbotbase"
+	log "github.com/sirupsen/logrus"
 	tgbotapi "gopkg.in/telegram-bot-api.v4"
 )
 
 type findHandler struct {
 	tgbotbase.BaseHandler
 
-	cache *PicCache
+	cardsDir string
+	cache    *PicCache
 }
+
+const dumpFilename = "all.dump.json"
 
 var _ tgbotbase.IncomingMessageHandler = &findHandler{}
 
-func NewFindHandler(cache *PicCache) tgbotbase.IncomingMessageHandler {
+func NewFindHandler(cardsDir string, cache *PicCache) tgbotbase.IncomingMessageHandler {
 	h := findHandler{
-		cache: cache,
+		cardsDir: cardsDir,
+		cache:    cache,
 	}
 	return &h
 }
 
+func loadDump(dumpPath string) error {
+	const url = "https://archive.scryfall.com/json/scryfall-all-cards.json"
+	log.WithFields(log.Fields{"url": url, "dumpFile": dumpPath}).Info("loading new dump")
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create the file
+	dumpTmp := dumpPath + ".tmp"
+	out, err := os.Create(dumpTmp)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if err := os.Rename(dumpTmp, dumpPath); err != nil {
+		return err
+	}
+	log.WithFields(log.Fields{"dumpFile": dumpPath}).Info("new dump has been downloaded")
+	return nil
+}
+
+type Cards []Card
+
+type Card struct {
+	ID              string `json:"id"`
+	Type            string `json:"type"`
+	Name            string `json:"name"`
+	Description     string `json:"description"`
+	PermalinkURI    string `json:"permalink_uri"`
+	UpdatedAt       string `json:"updated_at"`
+	CompressedSize  int    `json:"compressed_size"`
+	ContentType     string `json:"content_type"`
+	ContentEncoding string `json:"content_encoding"`
+}
+
 func (h *findHandler) Init(outMsgCh chan<- tgbotapi.Chattable, srvCh chan<- tgbotbase.ServiceMsg) tgbotbase.HandlerTrigger {
+	dumpPath := path.Join(h.cardsDir, dumpFilename)
+	os.MkdirAll(h.cardsDir, os.ModePerm)
+	if _, err := os.Stat(dumpPath); os.IsNotExist(err) {
+		log.WithFields(log.Fields{"dumpPath": dumpPath}).Info("dump is absent, loading")
+		if err := loadDump(dumpPath); err != nil {
+			panic(err)
+		}
+
+	}
+	f, err := os.Open(dumpPath)
+	if err != nil {
+		panic(err)
+	}
+	data, _ := ioutil.ReadAll(f)
+	var cards Cards
+	log.WithFields(log.Fields{"dumpPath": dumpPath, "size": len(data)}).Info("unmarshalling dump")
+	json.Unmarshal(data, &cards)
+	log.WithFields(log.Fields{"cards": len(cards)}).Info("unmarshalling done")
+
 	h.OutMsgCh = outMsgCh
 	return tgbotbase.NewHandlerTrigger(nil, []string{"find"})
 }
--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
 
 	log "github.com/sirupsen/logrus"
 
+	"./bot"
 	"github.com/admirallarimda/tgbotbase"
-	"github.com/ilyalavrinov/tgbot-mtg/bot"
 	"gopkg.in/gcfg.v1"
 )
 
@@ -14,6 +14,10 @@ var argCfg = flag.String("cfg", "./mtgbot.cfg", "path to config")
 
 type config struct {
 	tgbotbase.Config
+
+	Cards struct {
+		ScryfallDumpDir string
+	}
 
 	Cache struct {
 		Dir string
@@ -26,12 +30,18 @@ func main() {
 	var cfg config
 
 	if err := gcfg.ReadFileInto(&cfg, *argCfg); err != nil {
-		log.WithFields(log.Fields{"filepath": argCfg, "error": err}).Fatal("Config parse failed")
+		log.WithFields(log.Fields{"filepath": *argCfg, "error": err}).Fatal("Config parse failed")
 	}
 
-	tgbot := tgbotbase.NewBot(tgbotbase.Config{TGBot: cfg.TGBot})
+	tgbot := tgbotbase.NewBot(tgbotbase.Config{TGBot: cfg.TGBot, Proxy_SOCKS5: cfg.Proxy_SOCKS5})
+	if cfg.Cache.Dir == "" {
+		cfg.Cache.Dir = "./piccache"
+	}
+	if cfg.Cards.ScryfallDumpDir == "" {
+		cfg.Cards.ScryfallDumpDir = "./scryfall"
+	}
 
-	tgbot.AddHandler(tgbotbase.NewIncomingMessageDealer(bot.NewFindHandler(bot.NewPicCache(cfg.Cache.Dir))))
+	tgbot.AddHandler(tgbotbase.NewIncomingMessageDealer(bot.NewFindHandler(cfg.Cards.ScryfallDumpDir, bot.NewPicCache(cfg.Cache.Dir))))
 
 	log.Info("Starting bot")
 	tgbot.Start()
