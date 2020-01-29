dc68fd9d2c98594b9bf1115205aa583d5628e38f
Ilya Lavrinov <ilya.v.lavrinov@gmail.com>
2020-01-30 00:20:51 +0300

    Basic initial version

--- bot/find_handler.go
+++ bot/find_handler.go
@@ -2,11 +2,13 @@ package bot
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"path"
+	"regexp"
 
 	"github.com/admirallarimda/tgbotbase"
 	log "github.com/sirupsen/logrus"
@@ -18,6 +20,9 @@ type findHandler struct {
 
 	cardsDir string
 	cache    *PicCache
+
+	cardsByID   map[string]Card
+	cardsByName map[string]Card
 }
 
 const dumpFilename = "all.dump.json"
@@ -26,8 +31,10 @@ var _ tgbotbase.IncomingMessageHandler = &findHandler{}
 
 func NewFindHandler(cardsDir string, cache *PicCache) tgbotbase.IncomingMessageHandler {
 	h := findHandler{
-		cardsDir: cardsDir,
-		cache:    cache,
+		cardsDir:    cardsDir,
+		cache:       cache,
+		cardsByID:   make(map[string]Card),
+		cardsByName: make(map[string]Card),
 	}
 	return &h
 }
@@ -62,19 +69,23 @@ func loadDump(dumpPath string) error {
 	return nil
 }
 
-type Cards []Card
+func (h *findHandler) loadCards(cards []Card) {
+	for _, c := range cards {
+		h.cardsByID[c.ID] = c
+		h.cardsByName[c.Name] = c
+		h.cardsByName[c.LocalName] = c
+	}
+}
 
 type Card struct {
-	ID              string `json:"id"`
-	Type            string `json:"type"`
-	Name            string `json:"name"`
-	Description     string `json:"description"`
-	PermalinkURI    string `json:"permalink_uri"`
-	UpdatedAt       string `json:"updated_at"`
-	CompressedSize  int    `json:"compressed_size"`
-	ContentType     string `json:"content_type"`
-	ContentEncoding string `json:"content_encoding"`
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	LocalName   string `json:"printed_name"`
+	Lang        string `json:"lang"`
+	ScryfallURL string `json:"scryfall_uri"`
 }
+
+var re = regexp.MustCompile("\\[\\[(.*)\\]\\]")
 
 func (h *findHandler) Init(outMsgCh chan<- tgbotapi.Chattable, srvCh chan<- tgbotbase.ServiceMsg) tgbotbase.HandlerTrigger {
 	dumpPath := path.Join(h.cardsDir, dumpFilename)
@@ -91,17 +102,30 @@ func (h *findHandler) Init(outMsgCh chan<- tgbotapi.Chattable, srvCh chan<- tgbo
 		panic(err)
 	}
 	data, _ := ioutil.ReadAll(f)
-	var cards Cards
+	var cards []Card
 	log.WithFields(log.Fields{"dumpPath": dumpPath, "size": len(data)}).Info("unmarshalling dump")
 	json.Unmarshal(data, &cards)
 	log.WithFields(log.Fields{"cards": len(cards)}).Info("unmarshalling done")
 
+	h.loadCards(cards)
+
 	h.OutMsgCh = outMsgCh
-	return tgbotbase.NewHandlerTrigger(nil, []string{"find"})
+	return tgbotbase.NewHandlerTrigger(re, []string{"find"})
 }
 
 func (h *findHandler) HandleOne(msg tgbotapi.Message) {
-
+	cardname := ""
+	if msg.IsCommand() {
+		cardname = msg.CommandArguments()
+	} else {
+		cardname = re.FindString(msg.Text)
+	}
+	log.WithFields(log.Fields{"cardname": cardname, "msg": msg.Text}).Info("message triggered")
+	if c, found := h.cardsByName[cardname]; found {
+		h.OutMsgCh <- tgbotapi.NewMessage(msg.Chat.ID, fmt.Sprintf("card for name %q has been found. URL: %s", cardname, c.ScryfallURL))
+	} else {
+		h.OutMsgCh <- tgbotapi.NewMessage(msg.Chat.ID, fmt.Sprintf("could not find a card for name %q", cardname))
+	}
 }
 
 func (h *findHandler) Name() string {
